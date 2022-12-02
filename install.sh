fcdc97c73d22e72760def8119f9026a53f4d057d
琪露诺酱 <46920034+Rcrwrate@users.noreply.github.com>
2022-12-02 06:29:56 +0000

    Pending changes exported from your codespace

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 python -m venv venv
+source ./venv/bin/activate
 pip install nonebot
 pip install nonebot_adapter_mirai2
 pip install requests
 pip install nonebot_plugin_apscheduler
+# pip install cn2an
+# pip install aiohttp
+# pip install cachetools
+# pip install Pillow
+pip install bs4
+cd plugins/nonebot_plugin_gamedraw
+poetry install
+cd ../../
+python bot.py
