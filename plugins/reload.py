28d4d2522816f6c917247c5b66633e34a70a3f12
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2022-12-08 09:53:02 +0800

    HTTP API重启还是要的（）

--- plugins/reload.py
+++ plugins/reload.py
@@ -3,7 +3,7 @@ from nonebot import require
 from nonebot.log import logger
 from .__reboot.rebot import Reloader
 from nonebot import on_keyword
-# from Lib.Bot import BOT
+from Lib.Bot import BOT
 
 require("nonebot_plugin_apscheduler")
 reload = on_keyword(["Reload"], priority=1, block=True)
@@ -21,10 +21,10 @@ async def _reload():
     # nonebot.run(app="__mp_main__:app")
 
 
-# @scheduler.scheduled_job("cron", hour="*/4", id="reload_http")
-# async def _reload_http():
-#     logger.info("Reloading http!")
-#     b = BOT()
-#     b.reset()
-#     r = b.verify()
-#     b.bind(r["session"])
+@scheduler.scheduled_job("cron", hour="*/4", id="reload_http")
+async def _reload_http():
+    logger.info("Reloading http!")
+    b = BOT()
+    b.reset()
+    r = b.verify()
+    b.bind(r["session"])
