6fc118c94010916fd9bfe3ce45d356c98378688c
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2023-02-10 16:37:08 +0800

    库更新（Log）

--- Lib/log.py
+++ Lib/log.py
@@ -15,7 +15,7 @@ class Log():
     def __init__(self, name, log_level=INFO, log_path=".log") -> None:
         if os.path.exists(log_path) != True:
             os.mkdir(log_path)
-        
+
         self.LOG = logging.getLogger(name)
         self.LOG.setLevel(log_level)
         path = os.path.join(f"{log_path}", f"{name}.log")
@@ -38,18 +38,21 @@ class Log():
 
     def debug(self, msg, **kwargs):
         self.LOG.debug(msg, **kwargs)
-    
+
     def warn(self, msg, **kwargs):
         self.LOG.warn(msg, **kwargs)
 
     def error(self, msg, **kwargs):
         self.LOG.error(msg, **kwargs)
-    
+
     def critical(self, msg, **kwargs):
         self.LOG.critical(msg, **kwargs)
 
     def __del__(self):
         self.LOG.removeHandler(self.F)
+
+    def __getattr__(self, name):
+        return self.LOG.__getattribute__(name)
 
     warning = warn
     fatal = critical
